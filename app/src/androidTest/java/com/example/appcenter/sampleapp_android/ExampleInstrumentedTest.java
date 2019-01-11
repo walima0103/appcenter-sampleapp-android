5d4c08745fd875866482e500f62c988704eefe27
Wanderson Lima <walima@microsoft.com>
2019-01-11 01:52:45 -0200

    v0.2

--- app/src/androidTest/java/com/example/appcenter/sampleapp_android/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/appcenter/sampleapp_android/ExampleInstrumentedTest.java
@@ -4,10 +4,16 @@ import android.content.Context;
 import android.support.test.InstrumentationRegistry;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.After;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import static org.junit.Assert.*;
+
+import android.support.test.runner.AndroidJUnit4;
+import com.microsoft.appcenter.espresso.Factory;
+import com.microsoft.appcenter.espresso.ReportHelper;
 
 /**
  * Instrumentation test, which will execute on an Android device.
@@ -16,11 +22,21 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+    @Rule
+    public ReportHelper reportHelper = Factory.getReportHelper();
+
     @Test
     public void useAppContext() throws Exception {
+        reportHelper.label("Starting App");
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.example.appcenter.sampleapp_android", appContext.getPackageName());
+        reportHelper.label("Testing");
+    }
+
+    @After
+    public void TearDown(){
+        reportHelper.label("Stopping App");
     }
 }
